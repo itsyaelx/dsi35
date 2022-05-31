e0ebd13713e7be0e77e2b2e87c98886bfa0bb627
Marco A <Marco.Alarcon.R@outlook.com>
2022-05-30 19:06:12 -0500

    subida de archivos

--- IConductores.php
+++ IConductores.php
@@ -1,12 +1,13 @@
 <?php
 
+
     $Id=$_POST['Id']; 
     $Domicilio=$_POST['Domicilio'];
     $GrupoSanguineo=$_POST['GrupoSanguineo'];
     $Nombre=$_POST['Nombre'];
     $FechaNacimiento=$_POST['FechaNacimiento'];
-    $Firma=$_POST['Firma'];
-    $Foto=$_POST['Foto'];
+    //$Firma=$_POST['Firma'];
+    //$Foto=$_POST['Foto'];
     $Donador=$_POST['Donador'];
     $Antiguedad=$_POST['Antiguedad'];
 
@@ -17,16 +18,55 @@
     print("GrupoSanguineo: ".$GrupoSanguineo."<br>");
     print("Nombre: ".$Nombre."<br>");
     print("FechaNacimiento: ".$FechaNacimiento."<br>");
-    print("Firma: ".$Firma."<br>");
-    print("Foto: ".$Foto."<br>");
+    //print("Firma: ".$Firma."<br>");
+    //print("Foto: ".$Foto."<br>");
     print("Donador: ".$Donador."<br>");
     print("Antiguedad: ".$Antiguedad."<br>");
+    //echo $_FILES["Firma"]["type"];
 
-    include("Conexion.php");
-    $Con=Conectar();
-    $SQL="INSERT INTO Conductores(Id,Domicilio,GrupoSanguineo,Nombre,FechaNacimiento,Firma,Foto,Donador,Antiguedad) VALUES('$Id','$Domicilio','$GrupoSanguineo','$Nombre','$FechaNacimiento','$Firma','$Foto','$Donador','$Antiguedad')";
-    $Result=Ejecutar($Con,$SQL);
-    Desconectar($Con);
+    if($_FILES["Foto"]["error"]) {
+        echo "Se ha producido un error" . $_FILES["Foto"]["error"];
+    } else {
+        if($_FILES["Firma"]["type"]=="image/jpg" || $_FILES["Firma"]["type"]=="image/jpeg" ) {
+            if(move_uploaded_file($_FILES["Foto"]["tmp_name"],"C:\\xampp\htdocs\Proyecto Final\dsi35\imagenes\FotosConductores\Foto".$Id.".jpg")) {
+
+            } else {
+                echo "No se pudo mover el archivo";
+            }
+        } else {
+            echo "Seleccione una imagen jpg";
+        }
+
+    }
+
+    $Foto = "C:\\\\xampp\\\\htdocs\\\\Proyecto Final\\\\dsi35\\\\imagenes\\\\FotosConductores\\\\Foto".$Id.".jpg";
+
+
+    
+    if($_FILES["Firma"]["error"]) {
+        echo "Se ha producido un error" . $_FILES["Firma"]["error"];
+    } else {
+        if($_FILES["Firma"]["type"]=="image/jpg" || $_FILES["Firma"]["type"]=="image/jpeg" ) {
+            if(move_uploaded_file($_FILES["Firma"]["tmp_name"],"C:\\xampp\htdocs\Proyecto Final\dsi35\imagenes\FirmasConductores\Firma".$Id.".jpg")) {
+                
+                $Firma = "C:\\\\xampp\\\\htdocs\\\\Proyecto Final\\\\dsi35\\\\imagenes\\\\FotosConductores\\\\Firma".$Id.".jpg";
+
+                include("Conexion.php");
+                $Con=Conectar();
+                $SQL="INSERT INTO Conductores(Id,Domicilio,GrupoSanguineo,Nombre,FechaNacimiento,Firma,Foto,Donador,Antiguedad) VALUES('$Id','$Domicilio','$GrupoSanguineo','$Nombre','$FechaNacimiento','$Firma','$Foto','$Donador','$Antiguedad')";
+                $Result=Ejecutar($Con,$SQL);
+                Desconectar($Con);
+            } else {
+                echo "No se pudo mover el archivo";
+            }
+        } else {
+            echo "Seleccione una imagen jpg";
+        }
+
+    }
+
+
+
 
 
 ?>
--- ILicencias.php
+++ ILicencias.php
@@ -5,14 +5,14 @@
 //No es necesario declarar las variables
 //Las variables, constantes y arreglos llevan un "$"
 
-    //$NumLicencia=$_GET['Numero']; //Ve y saca el valor de lo que tenga el id de NumLicencia
+    $NumLicencia=$_GET['Numero']; //Ve y saca el valor de lo que tenga el id de NumLicencia
     $Tipo=$_GET['Tipo'];
     $FechaExp=$_GET['FechaExp'];
     $FechaVencimiento=$_GET['FechaVencimiento'];
     $Restriccion=$_GET['Restriccion'];
     $IdConductor=$_GET['IdConductor'];
 
-    //print("Licencia: ".$NumLicencia."<br>"); //"." Se usa para concatenar
+    print("Licencia: ".$NumLicencia."<br>"); //"." Se usa para concatenar
     print("Tipo: ".$Tipo."<br>");
     print("Fecha Exp: ".$FechaExp."<br>");
     print("Fecha Vencimiento: ".$FechaVencimiento."<br>");
@@ -22,7 +22,7 @@
     //Enviar datos al SMBD
     include("Conexion.php");
     $Con=Conectar();
-    $SQL="INSERT INTO Licencias VALUES('NULL','$Tipo','$FechaExp','$FechaVencimiento','$Restriccion','$IdConductor')";
+    $SQL="INSERT INTO Licencias VALUES('$NumLicencia','$Tipo','$FechaExp','$FechaVencimiento','$Restriccion','$IdConductor')";
     $Result=Ejecutar($Con,$SQL);
     Desconectar($Con);
 
