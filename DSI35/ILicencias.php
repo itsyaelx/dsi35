ae4b27b8fce74af499f4430daade5c3ff99594af
itsyaelx <ruizyael54@gmail.com>
2022-05-31 02:34:59 -0500

    archivos xml y pdf

--- DSI35/ILicencias.php
+++ DSI35/ILicencias.php
@@ -6,6 +6,7 @@
 //Las variables, constantes y arreglos llevan un "$"
 
     //$NumLicencia=$_GET['Numero']; //Ve y saca el valor de lo que tenga el id de NumLicencia
+    require('fpdf.php');
     $Tipo=$_GET['Tipo'];
     $FechaExp=$_GET['FechaExp'];
     $FechaVencimiento=$_GET['FechaVencimiento'];
@@ -13,17 +14,194 @@
     $IdConductor=$_GET['IdConductor'];
 
     //print("Licencia: ".$NumLicencia."<br>"); //"." Se usa para concatenar
-    print("Tipo: ".$Tipo."<br>");
-    print("Fecha Exp: ".$FechaExp."<br>");
-    print("Fecha Vencimiento: ".$FechaVencimiento."<br>");
-    print("Restriccion: ".$Restriccion."<br>");
-    print("IdConductor: ".$IdConductor."<br>");
+    //
 
     //Enviar datos al SMBD
     include("Conexion.php");
     $Con=Conectar();
     $SQL="INSERT INTO Licencias VALUES('NULL','$Tipo','$FechaExp','$FechaVencimiento','$Restriccion','$IdConductor')";
     $Result=Ejecutar($Con,$SQL);
+
+
+    //Creación de XML
+    $SQL="SELECT MAX(Numero) as Numero FROM Licencias";
+    $Result=Ejecutar($Con,$SQL);
+    $Fila = mysqli_fetch_row($Result);
+    $NumLicencia = $Fila[0];
+
+    $Manejador = fopen($NumLicencia.".xml", "w");
+
+    $Texto = "<NumLicencia>".$NumLicencia."</NumLicencia> \n".
+            "<Tipo>".$Tipo."</Tipo> \n".
+            "<FechaExp>".$FechaExp."</FechaExp> \n".
+            "<FechaVencimiento>".$FechaVencimiento."</FechaVencimiento>\n".
+            "<Restriccion>".$Restriccion."</Restriccion>\n".
+            "<IdConductor>".$IdConductor."</IdConductor>\n";
+
+    fwrite($Manejador, $Texto);
+
+    fclose($Manejador);
+
+    //CREACIÓN DE PDF
+
+    $SQL="SELECT L.Numero, L.Tipo, L.FechaExp,
+    L.FechaVencimiento, L.Restriccion, C.Nombre, C.FechaNacimiento, 
+    C.Domicilio, C.GrupoSanguineo, C.Donador, C.Antiguedad
+        FROM Licencias L, Conductores C
+        WHERE L.Numero='$NumLicencia'
+        AND L.IdConductor=C.Id;";
+    $Result=Ejecutar($Con,$SQL);
+
+    $Fila=mysqli_fetch_row($Result);
+
+
+    $pdf = new FPDF('P','mm', array(54,85));
+    $pdf->SetAutoPageBreak(false);
+    $pdf->AddPage(); 
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(10,1);
+    $pdf->Cell(0,3,'Estados Unidos Mexicanos',0,1); 
+    $pdf->SetXY(10,3);
+    $pdf->Cell(0,3,utf8_decode('Poder Ejecutivo del Estado de Querétaro'),0,1);
+    $pdf->Image("Logo.jpg", 2,2,8,10);
+
+    $pdf->SetFont('Arial','B',5); 
+    $pdf->SetXY(10,6);
+    $pdf->Cell(0,3,'Secretaria de Seguridad Ciudadana',0,1); 
+    $pdf->SetFont('Arial','B',6); 
+    $pdf->SetXY(10,9);
+    $pdf->Cell(0,3,'Licencia para conducir',0,1); 
+
+    //Foto persona
+    //$pdf->SetXY(10,9);
+    $pdf->Image("Persona.jpg", 33,12,17,20);
+    $pdf->SetFont('Arial','',2); 
+    $pdf->SetXY(24,20);
+    $pdf->Cell(0,3,'No. de Licencia',0,1); 
+    $pdf->SetFont('Arial','B',9); 
+    $pdf->SetXY(12,23);
+    $pdf->Cell(20,3,$Fila[0],0,1,'R');
+    $pdf->SetFont('Arial','B',5); 
+    $pdf->SetXY(20,27);
+    $pdf->Cell(0,3,'Automovilista',0,1);
+    //Datos
+    $pdf->SetFont('Arial','',3); 
+    $pdf->SetXY(45,32);
+    $pdf->Cell(0,3,'Nombre',2,1); 
+
+    $pdf->SetFont('Arial','',9); 
+    $pdf->SetXY(35,35);
+    $pdf->MultiCell(15,3,$Fila[5],0,'R');
+
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(40,42);
+    $pdf->Cell(0,3,'Observaciones',0,1);
+
+    //Fechas
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(2,50);
+    $pdf->Cell(0,3,'Fecha de Nacimiento',0,1);
+    $pdf->SetFont('Arial','',8); 
+    $pdf->SetXY(2,53);
+    $pdf->Cell(0,3,$Fila[6],0,1);
+
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(2,57);
+    $pdf->Cell(0,3,'Fecha de Expedición',0,1);
+    $pdf->SetFont('Arial','',8); 
+    $pdf->SetXY(2,60);
+    $pdf->Cell(0,3,$Fila[2],0,1);
+
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(2,64);
+    $pdf->Cell(0,3,'Valida hasta',0,1);
+    $pdf->SetFont('Arial','B',8); 
+    $pdf->SetXY(2,67);
+    $pdf->Cell(5,3,$Fila[3],0,1);
+
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(2,70);
+    $pdf->Cell(0,3,'Antiguedad',0,1);
+    $pdf->SetFont('Arial','',9); 
+    $pdf->SetXY(2,73);
+    $pdf->Cell(0,3,$Fila[10],0,1);
+
+    $pdf->SetFont('Arial','',15); 
+    $pdf->SetXY(4,78);
+    $pdf->Cell(0,3,$Fila[1],0,1);
+
+    //Firma
+    $pdf->Image("Firma.jpg", 20,68,15,15);
+    $pdf->SetFont('Arial','',4); 
+    $pdf->SetXY(14,77);
+    $pdf->Cell(0,3,'AUTORIZADO PARA QUE LA PRESENTE SEA',0,1);
+    $pdf->SetXY(14,79);
+    $pdf->Cell(0,3,'RECABADA COMO GARANTIA DE INFRACCION',0,1);
+
+    //Reverso
+    $pdf->AddPage(); 
+    $pdf->Image("Emergencias.jpg", 2,2,12,7);
+    $pdf->Image("Denuncia.jpg", 40,2,12,7);
+    $pdf->SetFont('Arial','',10); 
+    $pdf->SetXY(16,4);
+    $pdf->Cell(0,3,'A123456789',0,1);
+
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(43,10);
+    $pdf->Cell(0,3,'Domicilio',0,1);
+    $pdf->SetFont('Arial','',6); 
+    $pdf->SetXY(39,12);
+    $pdf->Cell(15,3,$Fila[7],0,1,'R');
+
+
+
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(2,29);
+    $pdf->Cell(0,3,'Restricciones',0,0);
+    $pdf->SetFont('Arial','',6); 
+    $pdf->SetXY(2,31);
+    $pdf->Cell(0,3,$Fila[4],0,0);
+
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(40,29);
+    $pdf->Cell(0,3,'Grupo Sanguineo',0,0);
+    $pdf->SetFont('Arial','',6); 
+    $pdf->SetXY(43,31);
+    $pdf->Cell(10,3,$Fila[8],0,0,'R');
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(38,33);
+    $pdf->Cell(0,3,'Donador de Organos',0,0);
+    $pdf->SetFont('Arial','',6); 
+    $pdf->SetXY(43,35);
+    $pdf->Cell(10,3,$Fila[9],0,0,'R');
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(36,37);
+    $pdf->Cell(0,3,'Numero de Emergencias',0,0);
+    $pdf->SetFont('Arial','',6); 
+    $pdf->SetXY(34,39);
+    $pdf->Cell(0,3,'000-442-129-4492',0,0);
+
+    //Firma
+    $pdf->Image("Firma2.png", 37,46,12,12);
+    $pdf->SetFont('Arial','B',4); 
+    $pdf->SetXY(19,59);
+    $pdf->Cell(0,3,'M. EN AP. JUAN MARCOS GRANADOS TORRES',0,1);
+    $pdf->SetXY(21,61);
+    $pdf->Cell(0,3,'SECRETARIO DE SEGURIDAD CIUDADANA',0,1);
+
+    $pdf->SetXY(2,62);
+    $pdf->Cell(0,3,'Fundamento Legal',0,1);
+
+    $pdf->SetFont('Arial','',4); 
+    $pdf->SetXY(2,64);
+    $pdf->MultiCell(0,3,'Ausdlfhsdofhsipdfj dsjfiodsjf iodshfu sdh osd hodshf osdhgoisdhgoshdgo hw8egdsuh sdh osdhgodsh gs8dhg odshg dhg 
+    dsuigh disgid hogsdihgodsgoihdsoghdsioghdsofhdsohdsohg sgh sdg 
+    sd fhdsigdis gdsighsdg',0,1);
+
+    $pdf->Output('F', $NumLicencia.".pdf"); 
+
+
     Desconectar($Con);
+
 
 ?>
--- DSI35/IVehiculos.php
+++ DSI35/IVehiculos.php
@@ -21,28 +21,6 @@
     $Marca=$_REQUEST['Marca']; 
     $SubMarca=$_REQUEST['SubMarca'];
 
-
-    print("IdVehiculo: ".$IdVehiculo."<br>");
-    print("Origen: ".$Origen."<br>");
-    print("Color: ".$Color."<br>");
-    print("Cilindraje: ".$Cilindraje."<br>");
-    print("Capacidad: ".$Capacidad."<br>");
-    print("Puertas: ".$Puertas."<br>");
-    print("Asientos: ".$Asientos."<br>");
-    print("CodigoVehicular: ".$CodigoVehicular."<br>");
-    print("Combustible: ".$Combustible."<br>");
-    print("Linea: ".$Linea."<br>");
-    print("Transmision: ".$Transmision."<br>");
-    print("Clase: ".$Clase."<br>");
-    print("Tipo: ".$Tipo."<br>");
-    print("RFA: ".$RFA."<br>");
-    print("Modelo: ".$Modelo."<br>");
-    print("NumMotor: ".$NumMotor."<br>");
-    print("Placa: ".$Placa."<br>");
-    print("NumSerie: ".$NumSerie."<br>");
-    print("Marca: ".$Marca."<br>");
-    print("SubMarca: ".$SubMarca."<br>");
-
     include("Conexion.php");
     $Con=Conectar();
     $SQL="INSERT INTO Vehiculos (IdVehiculo,Origen,Color,Cilindraje,Capacidad,Puertas,Asientos,CodigoVehicular,
@@ -51,6 +29,144 @@
     $Result=Ejecutar($Con,$SQL);
     Desconectar($Con);
 
+    $Manejador = fopen($IdVehiculo.".xml", "w");
+
+    $Texto = "<IdVehiculo>".$IdVehiculo."</IdVehiculo>\n".
+            "<Origen>".$Origen."</Origen>\n".
+            "<Color>".$Color."</Color>\n".
+            "<Cilindraje>".$Cilindraje."</Cilindraje>\n".
+            "<Capacidad>".$Capacidad."</Capacidad>\n".
+            "<Puertas>".$Puertas."</Puertas>\n".
+            "<Asientos>".$Asientos."</Asientos>\n".
+            "<CodigoVehicular>".$CodigoVehicular."</CodigoVehicular>\n".
+            "<Combustible>".$Combustible."</Combustible>\n".
+            "<Linea>".$Linea."</Linea>\n".
+            "<Transmision>".$Transmision."</Transmision>\n".
+            "<Clase>".$Clase."</Clase>\n".
+            "<Tipo>".$Tipo."</Tipo>\n".
+            "<RFA>".$RFA."</RFA>\n".
+            "<Modelo>".$Modelo."</Modelo>\n".
+            "<NumMotor>".$NumMotor."</NumMotor>\n".
+            "<Placa>".$Placa."</Placa>\n".
+            "<NumSerie>".$NumSerie."</NumSerie>\n".
+            "<Marca>".$Marca."</Marca>\n".
+            "<SubMarca>".$SubMarca."</SubMarca>\n";
+
+    fwrite($Manejador, $Texto);
+
+    fclose($Manejador);
+    
+    require('fpdf.php');
+
+    $pdf = new FPDF('P','mm', array(54,85));
+    $pdf->SetAutoPageBreak(false);
+    $pdf->AddPage(); 
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(10,1);
+    $pdf->Cell(0,3,'Estados Unidos Mexicanos',0,1); 
+    $pdf->SetXY(10,3);
+    $pdf->Cell(0,3,utf8_decode('Poder Ejecutivo del Estado de Querétaro'),0,1);
+    $pdf->Image("Logo.jpg", 2,2,8,10);
+
+    $pdf->SetFont('Arial','B',5); 
+    $pdf->SetXY(10,6);
+    $pdf->Cell(0,3,'Secretaria de Seguridad Ciudadana',0,1); 
+    $pdf->SetFont('Arial','B',6); 
+    $pdf->SetXY(10,9);
+    $pdf->Cell(0,3,utf8_decode('Reporte de alta de vehículo'),0,1);
+
+    $pdf->SetFont('Arial','',3); 
+    $pdf->SetXY(37,15);
+    $pdf->Cell(0,3,utf8_decode(date("d-m-Y h:i a")." UTC"),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,18);
+    $pdf->Cell(0,3,utf8_decode('Id del vehículo: '.$IdVehiculo),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,21);
+    $pdf->Cell(0,3,utf8_decode('Origen: '.$Origen),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,24);
+    $pdf->Cell(0,3,utf8_decode('Color: '.$Color),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,27);
+    $pdf->Cell(0,3,utf8_decode('Cilindraje: '.$Cilindraje),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,30);
+    $pdf->Cell(0,3,utf8_decode('Capacidad: '.$Capacidad),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,33);
+    $pdf->Cell(0,3,utf8_decode('Puertas: '.$Puertas),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,36);
+    $pdf->Cell(0,3,utf8_decode('Asientos: '.$Asientos),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,39);
+    $pdf->Cell(0,3,utf8_decode('Codigo vehicular: '.$CodigoVehicular),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,42);
+    $pdf->Cell(0,3,utf8_decode('Combustible: '.$Combustible),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,45);
+    $pdf->Cell(0,3,utf8_decode('Linea: '.$Linea),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,48);
+    $pdf->Cell(0,3,utf8_decode('Transmision: '.$Transmision),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,51);
+    $pdf->Cell(0,3,utf8_decode('Clase: '.$Clase),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,54);
+    $pdf->Cell(0,3,utf8_decode('Tipo: '.$Tipo),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,57);
+    $pdf->Cell(0,3,utf8_decode('RFA: '.$RFA),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,60);
+    $pdf->Cell(0,3,utf8_decode('Modelo: '.$Modelo),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,63);
+    $pdf->Cell(0,3,utf8_decode('NumMotor: '.$NumMotor),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,66);
+    $pdf->Cell(0,3,utf8_decode('Placa: '.$Placa),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,69);
+    $pdf->Cell(0,3,utf8_decode('NumSerie: '.$NumSerie),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,72);
+    $pdf->Cell(0,3,utf8_decode('Marca: '.$Marca),2,1);
+
+    $pdf->SetFont('Arial','',5); 
+    $pdf->SetXY(1,75);
+    $pdf->Cell(0,3,utf8_decode('Submarca: '.$SubMarca),2,1);
+
+
+
+
+    $pdf->Output('F', $IdVehiculo.".pdf");
+
+
+    
+        
 
 ?>
 
